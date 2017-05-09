1b49b30560915fe6c5b29315d03b1d4996741446
Chris de Claverie <declaverie@gmail.com>
2017-05-09 16:13:50 +0200

    code minimal

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
-var http = require('http');
+// Dependencies
+var express = require('express')
+var fs      = require('fs')
+var https   = require('https');
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Salut tout le monde !');
+// HTTPS
+var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
+
+// Initialization
+var app = express()
+var server = https.createServer(credentials, app);
+
+
+// Routing
+app.get('/', function (req, res) {
+
+
+
+  res.send('Hello World!');
 });
-server.listen(8080);
+
+
+
+server.listen(8080, function () {
+  console.log('Backend server listening on port 8080');
+});
