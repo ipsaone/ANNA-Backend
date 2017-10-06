8fd0a2a8dcad2786368c4fa94aa07d6751723a25
boopr <louis.etienne82@orange.fr>
2017-10-06 14:55:24 +0200

    All method are asynchronous

--- repositories/Storage.js
+++ repositories/Storage.js
@@ -9,26 +9,47 @@ class Storage {
         return _path.join(__dirname, '..', config.storage.folder);
     }
 
-    static getObject(path) {
-        const type = this._guessType(path);
+    static getObject(path, cb) {
+        this._guessType(path, (err, type) => {
+            if (err) return cb(err);
 
-        console.log(type);
+            if (type === 'directory') {
+                return cb(null, 'directory');
+            }
+            else if (type === 'file') {
+                return cb(null, this._getFile(path));
+            }
+            else {
+                return cb(null, type);
+            }
+        });
+    }
+
+    static _getFile(path) {
+        return {
+            type: 'file',
+            path: _path.join(this.root, path)
+        };
     }
 
 
-    static _guessType(path) {
-        path = _path.join(this.root, path); // Create the complete file path
+    static _guessType(path, cb) {
+        fs.stat(_path.join(this.root, path), (err, stats) => {
+            if (err) return cb(err);
 
-        const stats = fs.statSync(path);
-
-        if(stats.isDirectory())
-            return 'directory';
-        else if(stats.isFile())
-            return 'file';
-        else
-            return Error('Unknown type');
+            if (stats.isDirectory())
+                return cb(null, 'directory');
+            else if (stats.isFile()) {
+                return cb(null, 'file');
+            }
+            else
+                return cb(null, 'ETYPE');
+        });
     }
 }
 
 // TESTS
-Storage.getObject('bar/');
+Storage.getObject('bar/foo.txt', (err, result) => {
+    if (err) console.log(err);
+    else console.log(result);
+});
