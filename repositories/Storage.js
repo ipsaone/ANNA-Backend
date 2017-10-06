c7148ea06fafda1295808119eb4e31c9ef25761e
boopr <louis.etienne82@orange.fr>
2017-10-06 14:26:12 +0200

    Add method for guessing the type of the object from the path

--- repositories/Storage.js
+++ repositories/Storage.js
@@ -1,14 +1,34 @@
 'use strict';
 
-const path = require('path');
+const fs = require('fs');
+const config = require('../config/config');
+const _path = require('path');
 
 class Storage {
     static get root() {
-        return path.join(__dirname, '..', 'storage');
+        return _path.join(__dirname, '..', config.storage.folder);
+    }
+
+    static getObject(path) {
+        const type = this._guessType(path);
+
+        console.log(type);
     }
 
 
+    static _guessType(path) {
+        path = _path.join(this.root, path); // Create the complete file path
+
+        const stats = fs.statSync(path);
+
+        if(stats.isDirectory())
+            return 'directory';
+        else if(stats.isFile())
+            return 'file';
+        else
+            return Error('Unknown type');
+    }
 }
 
 // TESTS
-console.log(Storage.root);
+Storage.getObject('bar/');
