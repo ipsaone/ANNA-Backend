98652e9f23cf5e471f4e5ed783688e05a5c46f22
Louis Etienne <louis.etienne82@orange.fr>
2017-10-11 22:28:07 +0200

    First commit for the log

---
+++ models/log.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+    const Log = sequelize.define('Log', {
+        title: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        content: {
+            allowNull: false,
+            type: DataTypes.TEXT
+        },
+        authorId: {
+            allowNull: false,
+            type: DataTypes.INTEGER
+        }
+    });
+
+    Log.associate = function (models) {
+        Log.belongsTo(models.User, {foreignKey: 'authorId', as: 'author'});
+        Log.hasOne(models.File, {foreignKey: 'logId', as: 'file'});
+    };
+    return Log;
+};
