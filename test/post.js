c621c49264325c3c287359f394fadb8a6f614c20
Louis Etienne <louis.etienne82@orange.fr>
2017-10-13 23:15:55 +0200

    New test system, cleaner and better

--- test/post.js
+++ test/post.js
@@ -4,14 +4,20 @@ const chai = require('chai');
 const db = require('../models');
 const server = require('../app');
 const expect = chai.expect;
+const fs = require('fs');
 
 chai.use(require('chai-http'));
 
 describe('Posts', () => {
     before(() => {
-        db.Post.destroy({where: {}});
-        db.Post.create({id: 1, title: 'Foo', markdown: 'Bar', authorId: 1, published: true});
-        db.Post.create({id: 2, title: 'Bar', markdown: 'Foo', authorId: 1, published: false});
+        return db.User.destroy({where: {}})
+            .then(db.User.create({id: 1, username: 'foo', password: 'secret', email: 'foo@local.dev'}))
+            .then(db.Post.destroy({where: {}}))
+            .then(() => {
+                db.Post.create({id: 1, title: 'Foo', markdown: 'Bar', authorId: 1, published: true});
+                db.Post.create({id: 2, title: 'Bar', markdown: 'Foo', authorId: 1, published: false});
+            })
+            .catch(err => console.err(err));
     });
 
 
@@ -120,6 +126,122 @@ describe('Posts', () => {
 
 // [POST] /posts
     describe('[POST] /posts', () => {
-        it('expect ');
-    })
+        it('expect POST to return the new post with status 201', done => {
+            chai.request(server)
+                .post('/posts')
+                .send({id: 3, title: 'Lorem ipsum', markdown: 'Lorem ipsum', authorId: 1, published: true})
+                .end((err, res) => {
+                    expect(err).to.be.null;
+
+                    expect(res).to.have.status(201);
+                    expect(res).to.be.json;
+
+                    expect(res.body.id);
+                    expect(res.body.title).to.be.equal('Lorem ipsum');
+                    expect(res.body.markdown).to.be.equal('Lorem ipsum');
+                    expect(res.body.content.replace(/\r?\n?/g, '')).to.be.equal('<p>Lorem ipsum</p>');
+
+                    done();
+                });
+        });
+
+        it('expect the new post to exist in the database', done => {
+            db.Post.findById(3)
+                .then(post => {
+                    expect(post.id).to.be.equal(3);
+                    expect(post.title).to.be.equal('Lorem ipsum');
+                    expect(post.markdown).to.be.equal('Lorem ipsum');
+                    expect(post.content.replace(/\r?\n?/g, '')).to.be.equal('<p>Lorem ipsum</p>');
+                    expect(post.published).to.be.true;
+                    expect(post.publishedAt).to.not.be.null;
+                    expect(post.createdAt).to.not.be.null;
+                    expect(post.updatedAt).to.not.be.null;
+
+                    done();
+                });
+        });
+
+        it('expect POST post to return an error with status 400 when sending an incomplete request', done => {
+            chai.request(server)
+                .post('/posts')
+                .send({id: 3, markdown: 'Lorem ipsum', authorId: 1, published: true}) // forgot the title
+                .end((err, res) => {
+                    expect(err).to.not.be.null;
+                    expect(res).to.have.status(400);
+
+                    done();
+                });
+        });
+    });
+
+// [PUT] /posts/:id
+    describe('[PUT] /posts/:id', () => {
+        it('expect PUT to update the post with id = 3 and return nothing with status 204', done => {
+            chai.request(server)
+                .put('/posts/3')
+                .send({title: 'Edited title', published: false})
+                .end((err, res) => {
+                    expect(err).to.be.null;
+
+                    expect(res).to.have.status(204);
+                    expect(res.body).to.be.empty;
+
+                    done();
+                });
+        });
+
+        it('expect the data to be updated in the database', done => {
+            db.Post.findById(3)
+                .then(post => {
+                    expect(post.title).to.be.equal('Edited title');
+                    expect(post.published).to.be.false;
+                    expect(post.publishedAt).to.be.null;
+                    expect(post.updatedAt).to.not.be.equal(post.createdAt);
+
+                    done();
+                });
+        });
+
+        it('expect PUT to return an error with the status 400 when sending an incomplete request', done => {
+            chai.request(server)
+                .put('/posts/3')
+                .send({title: null})
+                .end((err, res) => {
+                    expect(err).to.not.be.null;
+                    expect(res).to.have.status(400);
+
+                    done();
+                });
+        });
+    });
+
+// [DELETE] /posts/:id
+    describe('[DELETE] /posts/:id', () => {
+        it('expect to DELETE the post with id = 3 and return nothing with status 204', done => {
+            chai.request(server)
+                .delete('/posts/3')
+                .end((err, res) => {
+                    expect(err).to.be.null;
+
+                    expect(res).to.have.status(204);
+                    expect(res.body).to.be.empty;
+
+                    done();
+                });
+        });
+
+        it('expect to not have the post in the database', done => {
+            db.User.findById(3)
+                .then(post => {
+                    expect(post).to.be.null;
+
+                    done();
+                })
+                .catch(err => {
+                    expect(err).to.not.be.null;
+
+                    done();
+                });
+        });
+    });
 });
