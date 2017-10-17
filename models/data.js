311f7f54b944dce58dcdc47d29ec6b0db50b2f7e
Louis Etienne <louis.etienne82@orange.fr>
2017-10-17 14:17:43 +0200

    Adding missing informations

--- models/data.js
+++ models/data.js
@@ -5,6 +5,7 @@ module.exports = (sequelize, DataTypes) => {
         name: {allowNull: false, type: DataTypes.STRING},
         type: {allowNull: true, type: DataTypes.STRING},
         fileId: {allowNull: false, type: DataTypes.INTEGER},
+        dirId: {allowNull: false, default: 0, type: DataTypes.INTEGER},
         ownerId: {allowNull: false, type: DataTypes.INTEGER},
         rightsId: {allowNull: false, type: DataTypes.INTEGER},
         groupId: {allowNull: false, type: DataTypes.INTEGER},
@@ -15,6 +16,9 @@ module.exports = (sequelize, DataTypes) => {
         Data.belongsTo(models.User, {foreignKey: 'ownerId', as: 'author'});
         Data.belongsTo(models.Right, {foreignKey: 'rightsId', as: 'rights'});
         Data.belongsTo(models.Group, {foreignKey: 'groupId', as: 'group'});
+
+        Data.belongsTo(models.Data, {foreignKey: 'dirId', as: 'directory'});
+        Data.hasMany(models.Data, {foreignKey: 'dirId', as: 'files'});
     };
 
     return Data;
