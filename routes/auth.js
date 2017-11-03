5861e22140f325d487a3113b3c41ffadec851ea2
Louis Etienne <louis.etienne82@orange.fr>
2017-11-03 13:52:12 +0100

    Adding check method

--- routes/auth.js
+++ routes/auth.js
@@ -5,5 +5,6 @@ const auth_controller = require('../controllers/auth_controller');
 
 router.post('/login', auth_controller.login);
 router.get('/logout', auth_controller.logout);
+router.get('/check', auth_controller.check);
 
 module.exports = router;
