5ce8c64177ed1f6d3d160e16c0fcc5da3e105240
[IPSA] Chris de Claverie <declaverie@gmail.com>
2017-12-28 00:36:41 +0100

    Added unique constraint to migrations & script to auto-clear database

--- migrations/20171207141337-create-event.js
+++ migrations/20171207141337-create-event.js
@@ -8,22 +8,31 @@ module.exports = {
             primaryKey: true,
             type: Sequelize.INTEGER
         },
-        name: {type: Sequelize.STRING},
+        name: {
+            allowNull: false,
+            type: Sequelize.STRING,
+            defaultValue: Sequelize.NULL,
+            unique: true
+        },
         markdown: {
             allowNull: false,
-            type: Sequelize.TEXT
+            type: Sequelize.TEXT,
+            defaultValue: Sequelize.NULL
         },
         content: {
             allowNull: false,
-            type: Sequelize.TEXT
+            type: Sequelize.TEXT,
+            defaultValue: Sequelize.NULL
         },
         createdAt: {
             allowNull: false,
-            type: Sequelize.DATE
+            type: Sequelize.DATE,
+            defaultValue: Sequelize.NOW
         },
         updatedAt: {
             allowNull: false,
-            type: Sequelize.DATE
+            type: Sequelize.DATE,
+            defaultValue: Sequelize.NOW
         }
     }),
     down: (queryInterface) => queryInterface.dropTable('Events')
