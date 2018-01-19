785fa15a24ca95a5eee2a8843b7bd5fde466bea1
[IPSA] Chris de Claverie <declaverie@gmail.com>
2018-01-19 16:41:47 +0100

    Log controllers refactoring : multiple files

--- controllers/group/store.js
+++ controllers/group/store.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const db = require('../../models');
+
+/**
+ *
+ * Creates a new group and stores it.
+ *
+ * @param {Object} req - The user request.
+ * @param {Object} res - The response to be sent.
+ *
+ * @returns {Object} Promise.
+ *
+ */
+
+module.exports = async function (req, res) {
+    if (typeof req.body.name !== 'string') {
+
+        throw res.boom.badRequest();
+    }
+
+    /*
+     * To lower case to avoid security problems
+     * (users trying to create 'auTHOrs' group to gain rights)
+     */
+    req.body.name = req.body.name.toLowerCase();
+
+    const group = await db.Group.create(req.body);
+
+    try {
+        res.status(201).json(group);
+    } catch (err) {
+        if (err instanceof db.Sequelize.ValidationError) {
+            throw res.boom.badRequest(err);
+        }
+
+        throw err;
+    }
+};
