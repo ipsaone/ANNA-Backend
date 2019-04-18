38acbf93980651e8541a7ee7f637e0edab223178
MagixInTheAir <MagixInTheAir@users.noreply.github.com>
2019-04-18 15:35:04 +0200

    backup

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,14 +1,4 @@
-# VARIABLES
-OUTPUT_FOLDER="/home/vagrant"
-OUTPUT_FILE=backup_$(date +%m-%d-%Y).tgz
-MYSQL_USERNAME="root"
-MYSQL_PWD="secret"
-TMP_FOLDER="/tmp"
-MYSQL_CONF_FOLDER="/etc/mysql"
-NGINX_CONF_FOLDER="/etc/nginx"
-REDIS_CONF_FOLDER="/etc/redis"
-ANNA_FOLDER="/home/vagrant/ANNA-Backend"
-LOG="./backup_log"
+source settings.sh
 
 # CREATE TEMP FOLDER
 echo "creating temp folder"
---
+++ backup/settings-default.sh
@@ -0,0 +1,11 @@
+# VARIABLES
+OUTPUT_FOLDER="/home/vagrant"
+OUTPUT_FILE=backup_$(date +%m-%d-%Y).tgz
+MYSQL_USERNAME="root"
+MYSQL_PWD="secret"
+TMP_FOLDER="/tmp"
+MYSQL_CONF_FOLDER="/etc/mysql"
+NGINX_CONF_FOLDER="/etc/nginx"
+REDIS_CONF_FOLDER="/etc/redis"
+ANNA_FOLDER="/home/vagrant/ANNA-Backend"
+LOG="./backup_log"
