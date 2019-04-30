022a438a5fe9603a4192e037d3bcb327648cc9fb
MagixInTheAir <MagixInTheAir@users.noreply.github.com>
2019-04-30 13:35:35 +0200

    Implement group policies

--- src/modules/groups/group_policy.js
+++ src/modules/groups/group_policy.js
@@ -1,21 +1,51 @@
 'use strict';
 
 exports.filterList = async (transaction) => {
-    transaction.logger.warn("Implementation of group policy neeeded");
+    return true;
 };
 
 exports.filterShow = async (transaction) => {
-    transaction.logger.warn("Implementation of group policy neeeded");
+    return true;
 };
 
 exports.filterStore = async (transaction) => {
-    transaction.logger.warn("Implementation of group policy neeeded");
+    transaction.logger.info("Filtering group store");
+    const db = transaction.db;
+
+    transaction.logger.info("Finding user");
+    const user = await db.User.findByPk(transaction.info.userId);
+
+    transaction.logger.info("Checking if user is root");
+    if (user && await user.isRoot()) {
+        return true;
+    }
+    return false;
 };
 
 exports.filterUpdate = async (transaction) => {
-    transaction.logger.warn("Implementation of group policy neeeded");
+    transaction.logger.info("Filtering group update");
+    const db = transaction.db;
+
+    transaction.logger.info("Finding user");
+    const user = await db.User.findByPk(transaction.info.userId);
+
+    transaction.logger.info("Checking if user is root");
+    if (user && await user.isRoot()) {
+        return true;
+    }
+    return false;
 };
 
 exports.filterDelete = async (transaction) => {
-    transaction.logger.warn("Implementation of group policy neeeded");
+    transaction.logger.info("Filtering group delete");
+    const db = transaction.db;
+
+    transaction.logger.info("Finding user");
+    const user = await db.User.findByPk(transaction.info.userId);
+
+    transaction.logger.info("Checking if user is root");
+    if (user && await user.isRoot()) {
+        return true;
+    }
+    return false;
 }
