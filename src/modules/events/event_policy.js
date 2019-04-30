1dc06351f4d82ac914aac238fb7edffcc5c523fb
MagixInTheAir <MagixInTheAir@users.noreply.github.com>
2019-04-30 08:57:00 +0200

    Add logging to events policies

--- src/modules/events/event_policy.js
+++ src/modules/events/event_policy.js
@@ -2,23 +2,28 @@
 
 
 exports.filterIndex = async (transaction) => {
-    console.error("TODO :ADD LOGGING, ADD 'ORGANIZERS' GROUP TO MANAGE EVENTS RIGHTS");
+    console.error("TODO : ADD 'ORGANIZERS' GROUP TO MANAGE EVENTS RIGHTS");
+    transaction.logger.info("Filtering events list");
     return true;
 }
 
 
 exports.filterShow = async (transaction) => {
     console.error("TODO : ADD LOGGING, ADD 'ORGANIZERS' GROUP TO MANAGE EVENTS RIGHTS");
+    transaction.logger.info("Filtering events show");
     return true;
 };
 
 
 exports.filterStore = async (transaction, userId) => {
     const db = transaction.db;
+    transaction.logger.info("Filtering events store");
 
+    transaction.logger.info("Finding user");
     const user = await db.User.findByPk(userId);
-    console.error("TODO : ADD LOGGING, ADD 'ORGANIZERS' GROUP TO MANAGE EVENTS RIGHTS");
+    console.error("TODO : ADD 'ORGANIZERS' GROUP TO MANAGE EVENTS RIGHTS");
 
+    transaction.logger.info("Checking if user is root");
     if (user && await user.isRoot()) {
         return true;
     }
@@ -28,10 +33,13 @@ exports.filterStore = async (transaction, userId) => {
 
 exports.filterUpdate = async (transaction, userId) => {
     const db = transaction.db;
+    transaction.logger.info("Filtering events update");
 
+    transaction.logger.info("Finding user");
     const user = await db.User.findByPk(userId);
-    console.error("TODO : ADD LOGGING, ADD 'ORGANIZERS' GROUP TO MANAGE EVENTS RIGHTS");
+    console.error("TODO : ADD 'ORGANIZERS' GROUP TO MANAGE EVENTS RIGHTS");
 
+    transaction.logger.info("Checking if user is root");
     if (user && await user.isRoot()) {
         return true;
     }
@@ -42,10 +50,13 @@ exports.filterUpdate = async (transaction, userId) => {
 
 exports.filterDelete = async (transaction, userId) => {
     const db = transaction.db;
+    transaction.logger.info("Filtering events delete");
 
+    transaction.logger.info("Finding user");
     const user = await db.User.findByPk(userId);
     console.error("TODO : ADD LOGGING, ADD 'ORGANIZERS' GROUP TO MANAGE EVENTS RIGHTS");
 
+    transaction.logger.info("Checking if user is root");
     if (user && await user.isRoot()) {
         return true;
     }
@@ -55,35 +66,43 @@ exports.filterDelete = async (transaction, userId) => {
 
 exports.filterStoreRegistered = async (transaction, targetId, userId) => {
     const db = transaction.db;
+    transaction.logger.info("Filtering registered user storage");
 
-    console.error("TODO : ADD LOGGING, ADD 'ORGANIZERS' GROUP TO MANAGE EVENTS RIGHTS");
+    console.error("TODO : ADD 'ORGANIZERS' GROUP TO MANAGE EVENTS RIGHTS");
+    transaction.logger.warn("TODO : CHECK A PLACE IS AVAILABLE !");
 
     if (userId === targetId) {
+        transaction.logger.info("Self-assign authorized");
         return true;
     }
 
+    transaction.logger.info("Finding user");
     const user = await db.User.findByPk(userId);
-    const userIsAdmin = await user.isRoot();
 
+    transaction.logger.info("Checking if user is root");
+    const userIsAdmin = await user.isRoot();
     if (userIsAdmin) {
         return true;
     }
-
     return false;
 };
 
 exports.filterDeleteRegistered = async (transaction, targetId, userId) => {
     const db = transaction.db;
 
-    console.error("TODO : ADD LOGGING, ADD 'ORGANIZERS' GROUP TO MANAGE EVENTS RIGHTS");
+    console.error("TODO : ADD 'ORGANIZERS' GROUP TO MANAGE EVENTS RIGHTS");
+    transaction.logger.warn("TODO : CHECK A PLACE IS AVAILABLE !");
 
     if (userId === targetId) {
+        transaction.logger.info("Self-assign authorized");
         return true;
     }
 
+    transaction.logger.info("Finding user");
     const user = await db.User.findByPk(userId);
-    const userIsAdmin = await user.isRoot();
 
+    transaction.logger.info("Checking if user is root");
+    const userIsAdmin = await user.isRoot();
     if (userIsAdmin) {
         return true;
     }
