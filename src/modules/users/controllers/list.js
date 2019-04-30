96314a3abcbe50d153695e4ee92cfd4fdfed3fc5
MagixInTheAir <MagixInTheAir@users.noreply.github.com>
2019-04-30 16:22:28 +0200

    Fix user controller 'list' to use policies

--- src/modules/users/controllers/list.js
+++ src/modules/users/controllers/list.js
@@ -1,12 +1,13 @@
 'use strict';
 
-
+const policy = require('../user_policy');
 
 module.exports = (db) => async function (req, res) {
     req.transaction.logger.info('Finding all users');
     const users = await db.User.findAll();
 
+    let filteredData = await policy.filterIndex(db, users, req.session.id);
 
     req.transaction.logger.info('Sending users');
-    return res.status(200).json(users);
+    return res.status(200).json(filteredData);
 };
