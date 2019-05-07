7d4a001614e3604203a4468a34e697199914c281
MagixInTheAir <MagixInTheAir@users.noreply.github.com>
2019-05-07 11:19:08 +0200

    Improve module test to specify exact file

--- test/test_module.sh
+++ test/test_module.sh
@@ -1,1 +1,7 @@
-ava --verbose 'src/modules/'$1'/test/*.js'
+#!/bin/bash
+
+if [ -z $2 ]; then
+    ava --verbose 'src/modules/'$1'/test/*.js';
+else
+    ava --verbose 'src/modules/'$1'/test/'$2'.js';
+fi
