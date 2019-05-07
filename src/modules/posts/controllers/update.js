7ab1531cfc8db51962b045bda40e3ecdbf2982b0
MagixInTheAir <MagixInTheAir@users.noreply.github.com>
2019-05-07 09:39:38 +0200

    Add schema to post update controller

--- src/modules/posts/controllers/update.js
+++ src/modules/posts/controllers/update.js
@@ -1,14 +1,27 @@
 'use strict';
 
 const policy = require('../post_policy');
+const joi = require('joi');
 
+const schema = joi.object().keys({
+    title : joi.string().trim(true),
+    markdown : joi.string().trim(true),
+    authorId : joi.number(),
+    published : joi.boolean()
+});
 
 
 module.exports = (db) => async function (req, res, handle) {
     const postId = parseInt(req.params.postId, 10);
     req.transaction.logger.info('Post update controller invoked', {postId});
 
-    req.transaction.logger.warn('MISSING SCHEMA');
+    // Validate user input
+    req.transaction.logger.info('Validating input');
+    const validation = joi.validate(req.body, schema);
+    if (validation.error) {
+        req.transaction.logger.info('Input denied by validator');
+        return res.boom.badRequest(validation.error);
+    }
 
     req.transaction.logger.info('Checking policies');
     let authorized = await policy.filterUpdate(req.transaction, req.session.auth);
