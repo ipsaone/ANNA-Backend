a210eff95b5175818176e9d26684eb2e399ed7d5
MagixInTheAir <MagixInTheAir@users.noreply.github.com>
2019-05-07 11:56:42 +0200

    Small storage download controller refactor

--- src/modules/storage/controllers/download.js
+++ src/modules/storage/controllers/download.js
@@ -46,23 +46,25 @@ module.exports = (db) => async (req, res) => {
 
         req.transaction.logger.info('Sending file', {data: data.name});
         return res.download(dataPath, data.name);
+    } else {
+
+        req.transaction.logger.info('Starting metadata download routine');
+
+        req.transaction.logger.debug('Checking policy')
+        const allowed = await policy.filterDownloadMeta(req.transaction, fileId, req.session.auth);
+        if (!allowed) {
+            req.transaction.logger.info('Metadata download refused by policy');
+            throw res.boom.unauthorized();
+        }
+
+        req.transaction.logger.debug('Requesting corresponding data');
+        const data = await db.Data.findAll({
+            where: {fileId},
+            include: ['rights']
+        });
+
+        req.transaction.logger.info('Sending metadata', {data: data.map(e => e.toJSON())})
+        return res.status(200).json(data);
+
     }
-
-    req.transaction.logger.info('Starting metadata download routine');
-
-    req.transaction.logger.debug('Checking policy')
-    const allowed = await policy.filterDownloadMeta(req.transaction, fileId, req.session.auth);
-    if (!allowed) {
-        req.transaction.logger.info('Metadata download refused by policy');
-        throw res.boom.unauthorized();
-    }
-
-    req.transaction.logger.debug('Requesting corresponding data');
-    const data = await db.Data.findAll({
-        where: {fileId},
-        include: ['rights']
-    });
-
-    req.transaction.logger.info('Sending metadata', {data: data.map(e => e.toJSON())})
-    return res.status(200).json(data);
 };
