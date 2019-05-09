b55b9044069d9d9b9fa657ae99e5ada8aa3c0516
MagixInTheAir <MagixInTheAir@users.noreply.github.com>
2019-05-09 11:01:42 +0200

    Complete mission tasks tests

--- src/modules/missions/test/tasks.js
+++ src/modules/missions/test/tasks.js
@@ -30,6 +30,11 @@ test.beforeEach(async t => {
         password: 'password_test',
         email: 'test@test.com'
     });
+    t.context.user2 = await db.User.create({
+        username: 'login_test_2',
+        password: 'password_test_2',
+        email: 'test_2@test.com'
+    });
     let res = await request.post('/auth/login').send({
         username: 'login_test',
         password: 'password_test'
@@ -39,25 +44,25 @@ test.beforeEach(async t => {
     t.context.group = await db.Group.create({
         name: "root"
     });
+    
+});
 
+
+test('Add, show, list, edit and remove task from mission (root, not leader)', async t => {
     await t.context.user.addGroup(t.context.group.id);
-    let res2 = await t.context.request.post('/missions')
+    let res8 = await t.context.request.post('/missions')
         .send({
             name: "test", 
             markdown: "# TEST",
             budgetAssigned: 100,
             budgetUsed: 40,
-            groupId: t.context.user.id,
-            leaderId: t.context.group.id
+            groupId: t.context.group.id,
+            leaderId: t.context.user2.id 
         });
 
-    t.is(res2.status, 200);  
-    t.context.mission = res2.body;
-    
-});
+    t.is(res8.status, 200);  
+    t.context.mission = res8.body;
 
-
-test('Add, show, list, edit and remove task from mission', async t => {
     let res = await t.context.request.post('/missions/'+t.context.mission.id+'/tasks')
         .send({
             done: false,
@@ -93,5 +98,53 @@ test('Add, show, list, edit and remove task from mission', async t => {
     t.is(res4.body.length, 0);
 });
 
+test('Add, show, list, edit and remove task from mission (leader, not root)', async t => {
+    await t.context.user.addGroup(t.context.group.id);
+    let res8 = await t.context.request.post('/missions')
+        .send({
+            name: "test", 
+            markdown: "# TEST",
+            budgetAssigned: 100,
+            budgetUsed: 40,
+            groupId: t.context.group.id,
+            leaderId: t.context.user.id 
+        });
 
-test.todo('Add, show, list, edit and remove task from mission (not root)');
+    t.is(res8.status, 200);  
+    t.context.mission = res8.body;
+    await t.context.user.setGroups([]);
+
+    let res = await t.context.request.post('/missions/'+t.context.mission.id+'/tasks')
+        .send({
+            done: false,
+            name: 'test'
+        });
+    t.is(res.status, 200);
+
+    let res2 = await t.context.request.get('/missions/'+t.context.mission.id+'/tasks');
+    t.is(res2.status, 200);
+    t.is(res2.body.length, 1);
+    t.is(res2.body[0].name, 'test');
+
+    let res0 = await t.context.request.get('/missions/'+t.context.mission.id+'/task/'+res.body.id);
+    t.is(res0.status, 200);
+    t.is(res0.body.name, 'test');
+
+    let res6 = await t.context.request.put('/missions/'+t.context.mission.id+'/task/'+res.body.id)
+        .send({
+            name: 'testEdit'
+        });
+    t.is(res6.status, 200);
+   
+    let res5 = await t.context.request.get('/missions/'+t.context.mission.id+'/tasks');
+    t.is(res5.status, 200);
+    t.is(res5.body.length, 1);
+    t.is(res5.body[0].name, 'testEdit');
+
+    let res3 = await t.context.request.delete('/missions/'+t.context.mission.id+'/task/'+res.body.id);
+    t.is(res3.status, 204);
+
+    let res4 = await t.context.request.get('/missions/'+t.context.mission.id+'/tasks');
+    t.is(res4.status, 200);
+    t.is(res4.body.length, 0);
+});
