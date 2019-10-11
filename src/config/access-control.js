adcbec5aa0c5bebad7ddcd5a24da08f27f3a9a3d
vagrant <vagrant@ANNA-BACKEND>
2019-10-11 20:50:34 +0000

    Complete ACL

--- src/config/access-control.js
+++ src/config/access-control.js
@@ -13,18 +13,34 @@ module.exports = [
     ["store-mission", ["root"]],
     ["update-mission", ["root"]],
     ["delete-mission", ["root"]],
+
     ["index-tasks", ["default"]],
     ["show-task", ["default"]],
     ["store-task", ["root"]],
     ["update-task", ["root"]],
     ["delete-task", ["root"]],
+    
     ["add-member-mission", ["root"]],
     ["rem-member-mission", ["root"]],
 
+    ["index-users", ["default"]],
+    ["show-user", ["default"]],
+    ["store-user", ["root"]],
+    ["update-user", ["root"]],
+    ["delete-user", ["root"]],
+    ["index-user-groups", ["default"]],
+    ["store-user-group", ["root"]],
+    ["delete-user-group", ["root"]],
 
+    ["index-groups", ["default"]],
+    ["show-group", ["default"]],
+    ["store-group", ["root"]],
+    ["update-group", ["root"]],
+    ["delete-group", ["root"]],
 
-    ["handle-missions", ["root"]],
-    ["handle-groups", ["root"]],
-    ["handle-events", ["root", "organizers"]]
-
+    ["index-events", ["default"]],
+    ["show-event", ["default"]],
+    ["store-event", ["root", "organizers"]],
+    ["update-event", ["root", "organizers"]],
+    ["delete-event", ["root", "organizers"]]
 ]
