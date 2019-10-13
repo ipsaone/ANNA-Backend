5c66f09836faef2dd2e51564d84075a66c6f74b7
vagrant <vagrant@ANNA-BACKEND>
2019-10-13 09:32:59 +0000

    Add input validation to posts

--- src/modules/posts/controllers/delete.js
+++ src/modules/posts/controllers/delete.js
@@ -1,11 +1,22 @@
 'use strict';
 
 const policy = require('../post_policy');
+const joi = require('joi');
+const schema = joi.object().keys({});
 
 
 module.exports = (db) => async function (req, res) {
     const postId = parseInt(req.params.postId, 10);
     req.transaction.logger.info('Post deletion controller invoked', {postId});
+
+    // Validate user input
+    const validation = joi.validate(req.body, schema);
+    req.transaction.logger.debug('Validating schema');
+
+    if (validation.error) {
+        req.transaction.logger.debug('Bad input', {body : req.body});
+        return res.boom.badRequest(validation.error);
+    }
 
     req.transaction.logger.info('Deleting post');
     db.Post.destroy({where: {id: postId}});
--- src/modules/posts/controllers/show.js
+++ src/modules/posts/controllers/show.js
@@ -1,9 +1,20 @@
 'use strict';
 
 const policy = require('../post_policy');
+const joi = require('joi');
+const schema = joi.object().keys({});
 
 
 module.exports = (db) => async function (req, res, handle) {
+    // Validate user input
+    const validation = joi.validate(req.body, schema);
+    req.transaction.logger.debug('Validating schema');
+
+    if (validation.error) {
+        req.transaction.logger.debug('Bad input', {body : req.body});
+        return res.boom.badRequest(validation.error);
+    }
+
     const postId = parseInt(req.params.postId, 10);
     req.transaction.logger.info('Post details controller invoked', {postId});
 
