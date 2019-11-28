158b8c35de8ae7fd0627f947882610f9fc64414f
[IPSA] Chris de Claverie <chris.de-claverie@ipsa.fr>
2019-11-28 07:17:57 -0600

    One more try to fix deployment (#26)

---
+++ devops/pre-setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -o verbose
+
+if [[ "$TRAVIS_BRANCH" != "master" && "$TRAVIS_BRANCH" != "staging" ]]; then
+  echo "We're not on the master or staging branch."
+  exit -1
+fi
+
+if [[ -z "$encrypted_2861304911d5_key" && -z "$encrypted_2861304911d5_iv" ]]; then
+  echo "The encrypted variables aren't set."
+  exit -1
+fi
+
+openssl aes-256-cbc -K $encrypted_f57c64a0b291_key -iv $encrypted_f57c64a0b291_iv in ./travis_deploy_key.enc -out ~/travis_deploy_key -d
+eval "$(ssh-agent -s)"
+chmod 600 ~/travis_deploy_key
+echo -e "Host ipsaone.space\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+ssh-add ~/travis_deploy_key
+ssh -i ~/travis_deploy_key travis@ipsaone.space pwd
