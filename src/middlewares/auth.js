315916add724e5790e93ec3f8cd29d120eb26f21
kmgx <kmgx@declaverie.tech>
2019-12-06 07:43:21 +0800

    Refactor app.js and add static doc and coverage folders

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -7,7 +7,9 @@ const minimatch = require('minimatch');
 let authorized_paths = [
     '/',
     '/auth/*',
-    '/storage/files/([0-9]+)'
+    '/storage/files/([0-9]+)',
+    '/doc/*',
+    '/coverage/*'
 ]
 
 module.exports = (req, res, next) => {
